e1970050f3ab11c92884393e3614b7939457cf69
Jason Rigdon <jprigdon@gmail.com>
2017-04-09 13:18:18 +0000

    refactored functions to be more generic in vigenere

--- pset6/vigenere.py
+++ pset6/vigenere.py
@@ -8,38 +8,39 @@ lowercaseOffset = ord('a')
 uppercaseOffset = ord('A')
 
 def main():
-    
-    # output the enciphered plaintext
-    encipheredText = cipher_convert()
-    print("ciphertext: ", encipheredText)
 
-# prototype to check correct number of command-line arguments and if the cipherkey is valid
-def valid_cipherkey():
-    
     # checks if the correct number of command-line arguments are inputted
     if len(sys.argv) != validArguments:
         
         # warn user if they don't enter the correct number of command-line arguments and exit program
         print("Missing command-line argument")
         exit(1)
-    
+
     # local variable declaration
     cipherKey = sys.argv[1]
     
-    # iterate through each letter in the cipher key and make sure they are alphabetic characters
-    for i in range (len(cipherKey)):
-        
-        if cipherKey[i].isalpha() == False:
-            
-            # warn user if they input non-alphabetic characters and exit the program
-            print("Please enter alphanumeric characters only!")
-            exit(1)
+    if not IsCipherValid(cipherKey):
+        # warn user if they input non-alphabetic characters and exit the program
+        print("Please enter alphanumeric characters only!")
+        exit(1)
+
+    conversionString = GetUserString()
     
-    # upon a successful check, return the cipher key back to the function    
-    return cipherKey
+    # output the enciphered plaintext
+    encipheredText = cipher_convert(cipherKey, conversionString)
+    print("ciphertext: ", encipheredText)
+
+# prototype to check if the cipherkey is valid
+def IsCipherValid(cipher):
+    # iterate through each letter in the cipher key and make sure they are alphabetic characters
+    for iterationChar in cipher:
+        if not iterationChar.isalpha():
+            return False
+
+    return True
 
 # prototype to make sure the user inputs some plain text    
-def valid_plaintext():
+def GetUserString():
     
     plainText = None
     
@@ -53,29 +54,19 @@ def valid_plaintext():
     return plainText
 
 # prototype to encipher the plan text characters based on the cipher key    
-def cipher_convert():
-    
+def cipher_convert(cipherKey, plainText):
     # local variable declaration
-    cipherKey = valid_cipherkey()
     keyLength = len(cipherKey)
-    plainText = valid_plaintext()
     convertedString = ""
     cipherIterator = 0
     
     # iterates over each character in plain text
-    for i in range (len(plainText)):
-
+    for currentChar in plainText:
         # checks if the current character in the plain text is an alphabetical character or not
-        if plainText[i].isalpha():
-
-            # creates the cipher key character wraparound if it doesn't match the plain text input length
-            cipherCursor = cipherIterator % keyLength
-
-            # converts the current cipher key character to a lowercased integer offset value
-            cipherKeyValue = (ord(cipherKey[cipherCursor].lower()) - lowercaseOffset)
+        if currentChar.isalpha():
             
             # checks if the current character in the plain text is a lowercase character
-            if plainText[i].islower():
+            if currentChar.islower():
                 
                 # sets the offset to the lowercase value
                 offset = lowercaseOffset
@@ -86,8 +77,14 @@ def cipher_convert():
                 # sets the offset to the uppercase value
                 offset = uppercaseOffset
 
+            # creates the cipher key character wraparound if it doesn't match the plain text input length
+            cipherCursor = cipherIterator % keyLength
+
+            # converts the current cipher key character to a lowercased integer offset value
+            cipherKeyValue = (ord(cipherKey[cipherCursor].lower()) - lowercaseOffset)
+
             # offsets the current plain text character integer value by the cipher key integer value
-            newCharValue = ((ord(plainText[i]) + cipherKeyValue - offset) % alphabet) + offset
+            newCharValue = ((ord(currentChar) + cipherKeyValue - offset) % alphabet) + offset
 
             # converts the current enciphered integer value back to an alphabetical character and stores it in the array
             convertedString += chr(newCharValue)
@@ -99,7 +96,7 @@ def cipher_convert():
         else:
             
             # make no change to the current character in the plain text and store it in the array
-            convertedString += plainText[i]
+            convertedString += currentChar
     
     # return the values in the array back to the function        
     return convertedString
