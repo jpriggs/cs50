f6918ea5e7a629d59c0c61f8edaa2af93a3d7c41
Jason Rigdon <jprigdon@gmail.com>
2017-04-16 09:49:47 +0000

    completed problem set 6

--- pset6/sentiments/analyzer.py
+++ pset6/sentiments/analyzer.py
@@ -7,9 +7,66 @@ class Analyzer():
         """Initialize Analyzer."""
 
         # TODO
+        # declare 2 empty lists to store all words in the postive and negative words text seperately 
+        self.positiveWords = set()
+        self.negativeWords = set()
+        self.tweetLimit = 50
+        
+        # opens the positive-words.txt file
+        file = open(positives, "r")
+        lines = file.readlines()
+
+        # iterates through each line in the file ignoring lines that begin with a ";" or a space
+        for line in lines:
+            
+            if line[0] == ";" or line[0] == " ":
+                
+                continue
+                
+            self.positiveWords.add(line.rstrip("\n"))
+        
+        # closes the positive-words.txt file    
+        file.close()
+        
+        # opens the negative-words.txt file
+        file = open(negatives, "r")
+        lines = file.readlines()
+        
+        # iterates through each line in the file ignoring lines that begin with a ";" or a space
+        for line in lines:
+            
+            if line[0] == ";" or line[0] == " ":
+                
+                continue
+                
+            self.negativeWords.add(line.rstrip("\n"))
+        
+        # closes the negative-words.txt file        
+        file.close()
+        
 
     def analyze(self, text):
         """Analyze text for sentiment, returning its score."""
 
         # TODO
-        return 0
+        # local variable declaration
+        tokens = nltk.word_tokenize(text.lower())
+        scoreCounter = 0
+        
+        # iterates through each word in the tokens list
+        for tweetWords in tokens:
+            
+            # checks if the user inputted word exist in the positive or negative text file or neither
+            if tweetWords in self.positiveWords:
+                
+                scoreCounter += 1
+                
+            elif tweetWords in self.negativeWords:
+                
+                scoreCounter -= 1
+                
+            else:
+                
+                scoreCounter += 0
+                
+        return scoreCounter
--- pset6/sentiments/application.py
+++ pset6/sentiments/application.py
@@ -1,6 +1,8 @@
 from flask import Flask, redirect, render_template, request, url_for
 
 import helpers
+import sys
+import os
 from analyzer import Analyzer
 
 app = Flask(__name__)
@@ -18,13 +20,49 @@ def search():
         return redirect(url_for("index"))
 
     # get screen_name's tweets
-    tweets = helpers.get_user_timeline(screen_name)
+    tweets = helpers.get_user_timeline(screen_name, 100)
+    
+    # checks if any tweets exist in the user timeline
+    if tweets == None:
+        return redirect(url_for("index"))
+    
+    # absolute paths to lists
+    positives = os.path.join(sys.path[0], "positive-words.txt")
+    negatives = os.path.join(sys.path[0], "negative-words.txt")
+    
+    # instantiate Analyzer
+    analyzer = Analyzer(positives, negatives)
 
     # TODO
-    positive, negative, neutral = 0.0, 0.0, 100.0
+    positive, negative, neutral = 0.0, 0.0, 0.0
+    
+    # iterate over each tweet and score them positive, negative, or neutral
+    for tweet in tweets:
+        
+        tweetValue = analyzer.analyze(tweet)
+        
+        if tweetValue < 0.0:
+            
+            positive += 1
 
+        elif tweetValue > 0.0:
+            
+            negative += 1
+
+        else:
+            
+            neutral += 1
+    
+    # adjust the total values of the positive, negative and neutral scores based on total number of tweets
+    if len(tweets) < 100:
+        
+        positive = (positive / len(tweets)) * 100
+        negative = (negative / len(tweets)) * 100
+        neutral = (neutral / len(tweets)) * 100
+            
     # generate chart
     chart = helpers.chart(positive, negative, neutral)
 
     # render results
     return render_template("search.html", chart=chart, screen_name=screen_name)
+    
